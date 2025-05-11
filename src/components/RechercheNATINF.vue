c21227ba1b546d70bac749548dfb125247d897d5
retiolus <retiolus@tuta.io>
2025-05-11 12:59:31 +0200

    Abréviation dans les résultats

--- src/components/RechercheNATINF.vue
+++ src/components/RechercheNATINF.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { reactive, watch, defineProps, toRefs } from 'vue';
+import { correspondancesNatures } from './correspondancesNatures';
 
 const props = defineProps({
   multiple: {
@@ -32,17 +33,6 @@ watch(
     clearTimeout(recherche.recherche);
     recherche.recherche = setTimeout(async () => {
       recherche.enCours = true;
-      /*
-
-      exemple:
-      https://tabular-api.data.gouv.fr/api/resources/c6fb5b37-b3bc-4904-bd26-1b29ad7391df/data/?Unnamed:%202__contains=moyen*electronique
-
-      Selon que l'utilisateur entre du texte ou un nombre, on va rechercher l'ensemble des infractions qui contiennent le texte
-      ou l'infraction qui a exactement le code NATINF.
-
-      À ce jour la donnée qui figure sur le site data.gouv.fr est mal formatée, ce qui donne ce code difficilement compréhensible.
-
-      */
       let complementURL;
       const numericValue = parseFloat(val);
       if (!isNaN(numericValue) && isFinite(numericValue)) {
@@ -99,8 +89,7 @@ watch(
     <template #item="{ props: p, item }">
       <v-list-item
         v-bind="p"
-        prepend-icon="mdi-scale-balance"
-        :title="item.raw['Unnamed: 2']"
+        :title="`[${(correspondancesNatures[item.raw['Unnamed: 1']] || {}).abr || ''}] ${item.raw['Unnamed: 2']}`"
         :subtitle="item.raw['Ministère de la Justice, Direction des affaires criminelles et']"
       ></v-list-item>
     </template>
